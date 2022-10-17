3fe806a69f6717fd913d4f9a90686b87807d0958
Sirjamo1 <jam_how@hotmail.com>
2022-10-17 14:41:40 +1000

    added sound to ball drop

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ball = document.getElementById("ball");
 const eyeOne = document.getElementById("eye-one");
 const eyeTwo = document.getElementById("eye-two");
 const text = document.getElementById("text-bubble-text");
+let ballDropSound = new Audio("sounds/13959__adcbicycle__7.wav");
+let ballGotAwaySound = new Audio("sounds/630483__jimbo555__bouncing-ball.wav");
 let dialogue = [
     ["I have a ball. Perhaps you'd like to bounce it?"],
     ["Very good, for a first try"],
@@ -12,10 +14,12 @@ let dialogueIndex = 1;
 text.innerHTML = dialogue[0];
 ball.classList.add("ballWiggle");
 const theDrop = () => {
+    
     console.log("class added");
     eyeOne.classList.add("eyeDrop");
     eyeTwo.classList.add("eyeDrop");
     if (dialogueIndex === 2) {
+        ballGotAwaySound.play()
         ball.classList.add("ballGotAway");
         setTimeout(() => {
             console.log("ball got away");
@@ -28,6 +32,7 @@ const theDrop = () => {
             dialogueIndex = dialogueIndex >= 2 ? 0 : (dialogueIndex += 1);
         }, 1500);
     } else {
+        ballDropSound.play();
         ball.classList.add("ballDrop");
         setTimeout(() => {
             console.log(dialogueIndex);
